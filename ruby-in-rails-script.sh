8b06db4009bfc8f19c7116e9a6e8f2164c186e7f
csaument <c.s.aument@gmail.com>
2023-04-10 15:39:15 -0700

    initial commit

---
+++ ruby-in-rails-script.sh
@@ -0,0 +1,27 @@
+read -p "What is the application name?" app_name
+read -p "What is the application directory?" app_dir
+mkdir $app_dir
+cd $app_dir
+rails new $app_name -d postgresql -T
+cd $app_name
+rails db:create
+bundle add webpacker
+bundle add react-rails
+rails webpacker:install
+rails webpacker:install:react
+yarn add @babel/preset-react
+yarn add @rails/activestorage
+yarn add @rails/ujs
+rails g react:install
+rails g react:component App
+rails g controller Home index
+bundle add bootstrap
+mv app/assets/stylesheets/application.css app/assets/stylesheets/application.scss
+yarn add reactstrap
+mkdir /app/javascript/components/assets
+mkdir /app/javascript/components/components
+mkdir /app/javascript/components/pages
+touch /app/javascript/components/pages/Home.js
+touch /app/javascript/components/Navigation.js
+yarn add react-router-dom
+code .
